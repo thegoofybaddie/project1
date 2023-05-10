efeee81f48d6ed21b6a3c9f07b62e5182e54c21a
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-05-10 22:22:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dataframe import df
 from graph import box, histogram,pie
 from tab0 import student_list
 from tab1 import score
+from tab2 import kmeans
 
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
 
@@ -13,25 +14,13 @@ tabs_titles = ('Danh sách',
                'Phân nhóm',
                'Phân loại')
 tabs = st.tabs(tabs_titles)
-def checkbox(a,b):
-    if a:
-        pass
-    if b:
-        pass
-df2 = pd.DataFrame(columns = df.columns)
+
+
                 
 with tabs[0]:
     student_list()
-    
-    
 
-    # COLS = df.columns.values.tolist().copy()
-    # st.write(df[COLS[5]].value_counts())
-
-    
-
-    
-    
+   
 with tabs[1]:
     tabs1_titles = ('Số lượng hs',
                     'Điểm')
@@ -39,17 +28,16 @@ with tabs[1]:
     with tabs1[1]:
         score()
 
-    
-    
-    
-
-
 with tabs[2]:
-    # temp_options = ('low','medium','high')
-    # temp = st.select_slider('Temperature',options=temp_options)
-    # st.write('The temperature is:',temp)
+    kmeans()
+    
 
-    number = st.slider('Số nhóm',min_value=1,max_value=5,step=1)
+
+
+
+
+
+
 
 
 
